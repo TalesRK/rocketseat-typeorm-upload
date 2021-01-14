c29da043fa8c416dbd3e99ee0eb41d20e59e11de
Tales K <37679931+TalesRK@users.noreply.github.com>
2021-01-14 17:07:57 -0300

    final-tests

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,37 +1,59 @@
 // import AppError from '../errors/AppError';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 interface Request {
-    title: string;
-    type: 'income' | 'outcome';
-    value: number;
-    category: string;
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
 }
 
 class CreateTransactionService {
-    public async execute({
-        title,
-        value,
-        type,
-        category,
-    }: Request): Promise<Transaction> {
-        const transactionsRepository = getCustomRepository(
-            TransactionsRepository,
-        );
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
 
-        const transaction = transactionsRepository.create({
-            title,
-            value,
-            type,
-        });
+    const { total } = await transactionsRepository.getBalance();
 
-        await transactionsRepository.save(transaction);
-
-        return transaction;
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
     }
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+  }
 }
 
 export default CreateTransactionService;
