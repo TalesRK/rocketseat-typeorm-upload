591572df1c821421044d78ccca977fba9d635d6f
Tales K <37679931+TalesRK@users.noreply.github.com>
2021-01-14 13:15:42 -0300

    creating-transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,34 +1,36 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  return response.json({
-    transactions: [1, 2, 3],
-    balance: {
-      income: 8000,
-      outcome: 6000,
-      total: 2000,
-    },
-  });
-  // TODO
+    // TODO
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+    const { title, value, type, category } = request.body;
+    const createTransaction = new CreateTransactionService();
+
+    const transaction = await createTransaction.execute({
+        title,
+        value,
+        type,
+        category,
+    });
+
+    return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+    // TODO
 });
 
 transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+    // TODO
 });
 
 export default transactionsRouter;
